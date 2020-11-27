63017366d97ca05fa196fc4ff28d4ccc16abe85d
LIMADAVID <davidlima.dev@gmail.com>
2020-11-27 08:02:28 -0300

    Finalização projeto TheMovieApp

--- src/components/CarouselMulti.js
+++ src/components/CarouselMulti.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   View,
@@ -6,16 +6,14 @@ import {
   Dimensions,
   TouchableWithoutFeedback,
 } from 'react-native';
-import { Text, Title } from 'react-native-paper';
+import { Title } from 'react-native-paper';
 import Carousel from 'react-native-snap-carousel';
-import { map, size } from 'lodash';
 import { BASE_PATH_IMG } from '../utils/constants';
-import { getGenreMovieApi } from '../api/movies';
 
 const { width } = Dimensions.get('window');
-const ITEM_WIDTH = Math.round(width * 0.7);
+const ITEM_WIDTH = Math.round(width * 0.3);
 
-export default function CarouselVertical(props) {
+export default function CarouselMulti(props) {
   const { data, navigation } = props;
 
   return (
@@ -25,21 +23,17 @@ export default function CarouselVertical(props) {
       renderItem={(item) => <RenderItem data={item} navigation={navigation} />}
       sliderWidth={width}
       itemWidth={ITEM_WIDTH}
+      firstItem={1}
+      inactiveSlideScale={1}
+      inactiveSlideOpacity={1}
     />
   );
 }
 
 function RenderItem(props) {
   const { data, navigation } = props;
-  const { id, title, poster_path, genre_ids } = data.item;
-  const [genres, setGenres] = useState(null);
+  const { id, title, poster_path } = data.item;
   const imageUrl = `${BASE_PATH_IMG}/w500${poster_path}`;
-
-  useEffect(() => {
-    getGenreMovieApi(genre_ids).then((response) => {
-      setGenres(response);
-    });
-  }, []);
 
   const onNavigation = () => {
     navigation.navigate('movie', { id });
@@ -49,16 +43,9 @@ function RenderItem(props) {
     <TouchableWithoutFeedback onPress={onNavigation}>
       <View style={styles.card}>
         <Image style={styles.image} source={{ uri: imageUrl }} />
-        <Title style={styles.title}>{title}</Title>
-        <View style={styles.genres}>
-          {genres &&
-            map(genres, (genre, index) => (
-              <Text key={index} style={styles.genre}>
-                {genre}
-                {index !== size(genres) - 1 && ', '}
-              </Text>
-            ))}
-        </View>
+        <Title style={styles.title} numberOfLines={1}>
+          {title}
+        </Title>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -75,20 +62,13 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
   },
   image: {
-    width: '100%',
-    height: 450,
+    width: '85%',
+    height: 170,
     borderRadius: 20,
   },
   title: {
     marginHorizontal: 10,
     marginTop: 10,
-  },
-  genres: {
-    flexDirection: 'row',
-    marginHorizontal: 10,
-  },
-  genre: {
-    fontSize: 12,
-    color: '#8997a5',
+    fontSize: 16,
   },
 });
